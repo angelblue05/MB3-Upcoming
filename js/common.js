28f99aae2d6624de25201811061def8b5456d02b
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-24 02:37:02 -0600

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
+// Verify if deviceID exists
+chrome.storage.local.get(null, function(items) {
+  if (typeof items.deviceID === 'undefined') {
+    // the device id is not there, so let's generate one
+    device_id = guid();
+    // and then store it so it is there next time
+    chrome.storage.local.set({ deviceID: device_id });
+  } else {
+    // the device id is there, let's use it
+    device_id = items.deviceID;
+  }
+});
+
 // Generate a deviceID
 
 // Generate serial with 4 alphanumerics
