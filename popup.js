6da989b6da205ba40c9c21897ea4ccc899e822b2
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-24 00:34:27 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   /*$('#server-login').click(function() {
     $(this).hide();  
   });*/
+  
+  // When pressing connect
+  $('#save_settings').click(function() {
+    var $url = $('#setting_ip').val();
+    var $ip = $('#setting_port').val();
+    
+    $('#msgconnect').append("<p>Connecting to server!</p>")
+  });
 });
