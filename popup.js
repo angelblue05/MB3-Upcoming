2c0f03c6c24e6b238bbf57713d570b0f32ccf657
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-30 00:38:35 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		if (processing == 0) {  
 	        	// Prevent user from pressing connect multiple times.
 	        	processing = 1;
-	        	console.log("connect_stage1");
+
 	        	// Save user IP and port
 	        	var ip = $('#setting_ip').val();
 	        	var port = $('#setting_port').val();
@@ -26,16 +26,9 @@ $(document).ready(function() {
 		                chrome.storage.local.set({ ip: ip });
 		                chrome.storage.local.set({ port: port });
 		    
-		                // WORK IN PROGRESS
-		            
 		                // Display the list of users
-		                $.each(data, function(key, val) {
-		                    $('#userSelect').append(val['Name'] + "<br />\n")
-		                });
+		                getUsers();
 		                
-		                $("#server-login").fadeOut('slow');
-				$("#userSelect").delay(600).fadeIn('slow');
-	           
 	        	}).fail(function() { /* Testing failed */
 	        		$('#msgconnect').html("Unable to connect. Please verify your IP or URL and port.");
 	        	});
@@ -45,13 +38,17 @@ $(document).ready(function() {
 	});
 });
 
-function getUser() {
-	$('#msgconnect').hide();
-	$('#userSelect').show();
-  
-	$.getJSON(chrome.storage.local.get('ip') + ":" + chrome.storage.local.get('port') + "/mediabrowser/Users/AuthenticateByName", function(data) {
-		$("#users").html('');
+function getUsers() {
+	$("#server-login").fadeOut('slow');
+	$("#userSelect").delay(600).fadeIn('slow');
+
+	$.each(data, function(key, val) {
+		$('#userSelect').append(val['Name'] + "<br />\n")
 	});
+	
+	/*$.getJSON(chrome.storage.local.get('ip') + ":" + chrome.storage.local.get('port') + "/mediabrowser/Users/AuthenticateByName", function(data) {
+		$("#users").html('');
+	});*/
 }
 
 function headerSetup() {
