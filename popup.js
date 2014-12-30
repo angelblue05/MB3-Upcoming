1f2351151ff07fc5a450926d707003fd6a33ecea
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-30 00:50:09 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -46,7 +46,7 @@ function getUser() {
 		$.each(data, function(key, val) {
 			$('#userSelect').append(val['Name'] + "<br />\n")
 		});
-	}
+	});
 	
 	/*$.getJSON(chrome.storage.local.get('ip') + ":" + chrome.storage.local.get('port') + "/mediabrowser/Users/AuthenticateByName", function(data) {
 		$("#users").html('');
