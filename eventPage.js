968e35920dcd868a122f179dc7a5e4439215bcc2
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-30 16:23:33 -0600

    Update eventPage.js

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,11 @@
-/*
-*
-*/
+var cache = "";
+
+chrome.runtime.onMessage.addListener(function (request) {
+    if (request.cmd === "savestate") {
+        cache = $('body').html();
+    } else if (request.cmd === "getstate") {
+		if (cache != "") {
+			$('body').html(cache);
+		}
+	}
+}
