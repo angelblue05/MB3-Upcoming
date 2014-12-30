6980dfe172c6bafed361a790cac474000928d566
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-30 00:46:38 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -41,10 +41,12 @@ $(document).ready(function() {
 function getUsers() {
 	$("#server-login").fadeOut('slow');
 	$("#userSelect").delay(600).fadeIn('slow');
-
-	$.each(data, function(key, val) {
-		$('#userSelect').append(val['Name'] + "<br />\n")
-	});
+	
+	$.getJSON(chrome.storage.local.get('ip') + ":" + chrome.storage.local.get('port') + "/mediabrowser/Users/Public" + jsonf, function(data) {
+		$.each(data, function(key, val) {
+			$('#userSelect').append(val['Name'] + "<br />\n")
+		});
+	}
 	
 	/*$.getJSON(chrome.storage.local.get('ip') + ":" + chrome.storage.local.get('port') + "/mediabrowser/Users/AuthenticateByName", function(data) {
 		$("#users").html('');
