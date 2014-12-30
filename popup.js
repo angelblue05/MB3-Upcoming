135a4f528596699b40a0d4f4dec11b5c98a4da41
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-29 18:52:25 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,40 +1,48 @@
+var processing = 0;
+
 $(document).ready(function() {
   
   $('#userSelect').hide();
   // When pressing the connect button
-  $('#save_settings').click(function() {
+  if (processing == 0) {
+    var processing = 1;
     
-    // Save user IP and port
-    var ip = $('#setting_ip').val();
-    var port = $('#setting_port').val();
-    
-    // Verify if $ip contains http/https
-    if (ip.toLowerCase().indexOf('http://') == -1 && ip.toLowerCase().indexOf('https://') == -1) {
-      ip = 'http://' + ip;
-    }
-    
-    // Test the connection
-    $('#msgconnect').append("<p>Connecting to server...</p>"); /* Message */
-    
-    // Test with the given IP and port
-    $.getJSON(ip + ":" + port + "/mediabrowser/Users/Public", function(data) {
-      // Testing successful, save IP and port to storage
-      chrome.storage.local.set({ ip: ip });
-      chrome.storage.local.set({ port: port });
+    $('#save_settings').click(function() {
       
-      // WORK IN PROGRESS
-      // Verify if userID already exists
-      chrome.storage.local.get(null, function(items) {
-        // If userID doesn't exist, bring up user list
-        if (typeof items.user_id === 'undefined') {
-          getUser();  
-        } else {
-          headerSetup();
-        }
-      })
-      }).fail(function() { /* Testing connection failed */
-        $('#msgconnect').append("<p>Unable to connect. Please verify your IP or URL and port.</p>");
-      });
+      // Save user IP and port
+      var ip = $('#setting_ip').val();
+      var port = $('#setting_port').val();
+      
+      // Verify if $ip contains http/https
+      if (ip.toLowerCase().indexOf('http://') == -1 && ip.toLowerCase().indexOf('https://') == -1) {
+        ip = 'http://' + ip;
+      }
+      
+      // Test the connection
+      $('#msgconnect').append("<p>Connecting to server...</p>"); /* Message */
+      
+      // Test with the given IP and port
+      $.getJSON(ip + ":" + port + "/mediabrowser/Users/Public", function(data) {
+        // Testing successful, save IP and port to storage
+        chrome.storage.local.set({ ip: ip });
+        chrome.storage.local.set({ port: port });
+        
+        // WORK IN PROGRESS
+        // Verify if userID already exists
+        chrome.storage.local.get(null, function(items) {
+          // If userID doesn't exist, bring up user list
+          if (typeof items.user_id === 'undefined') {
+            getUser();  
+          } else {
+            headerSetup();
+          }
+        })
+        }).fail(function() { /* Testing connection failed */
+          $('#msgconnect').append("<p>Unable to connect. Please verify your IP or URL and port.</p>");
+        });
+    });
+    
+    var processing = 0;
   });
 });
 
