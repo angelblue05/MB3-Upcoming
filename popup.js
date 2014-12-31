d36788d89b3093d62235102ec9fb2a4356737263
angelblue05 <tamara.angel05@gmail.com>
2014-12-30 22:55:44 -0600

    userImage

--- popup.js
+++ popup.js
@@ -68,8 +68,9 @@ function getUser() {
 				if (typeof(val.PrimaryImageTag) != 'undefined') {
 					userImage = "background-image:url('"+ ipStorage +":"+ portStorage +"/mediabrowser/Users/"+val.Id+"/Images/Primary?width=100&tag="+val.PrimaryImageTag+"')";
 				}
-				userItems.push(userImage);
-				console.log(userImage);
+				// Add images to the userItems array
+				userItems.push("<div class=\"posterItemImage\" style=\"" + userImage + "\"></div>");
+
 				/*$('#userSelect').append(val['Name'] + "<br />\n");*/
 			}
 		});
