da7dfbc5ed19df827a76df40305860c06b4f2d63
angelblue05 <tamara.angel05@gmail.com>
2015-01-04 00:41:20 -0600

    Revert "Make popup persistent"
    This reverts commit ee63e7a71af600c2fb5db772bdd3626ef7c19861.

--- popup.js
+++ popup.js
@@ -1,24 +1,21 @@
 var processing = 0;
-var current;
 var jsonf = "?format=json";
+var port = null;
 
 
 $(document).ready(function() {
+	port = chrome.runtime.connect();
 
-	// Verify to load last loaded div by the user
-	chrome.storage.local.get('current', function(result) {
-		
-		if (result != 'undefined') {
-			current = result['current'];
-			window[current]();
-		} else {
-			// When first time running, setup IP
-			ipSetup();
-		}
+	port.onDisconnect.addListener(function() {
+		chrome.runtime.sendMessage({cmd: "savestate"});
 	});
+
+	chrome.runtime.sendMessage({cmd: "getstate"});
+
+	// When first time running, setup IP
+	ipSetup();
 	
 });
-
 
 function message(div, string) {
 	
@@ -28,9 +25,6 @@ function message(div, string) {
 
 function ipSetup() {
 
-
-	current = 'ipSetup';
-	chrome.storage.local.set({ 'current': current });
 	
 	// Reset ipSetup to default
 	$('#header_signIn').html('SIGN IN');
@@ -101,85 +95,76 @@ function ipSetup() {
 function getUser() {
 
 
-	current = 'getUser';
-	chrome.storage.local.set({ 'current': current });
+	$.getJSON(ipStorage + ":" + portStorage + "/mediabrowser/Users/Public" + jsonf, function(data) {
+		
+		// Reset getUser and userSelect/manualLogin divs
+		$('#userSelect').html('');
+		$('#header_signIn').html('<a id="back_ipSetup">BACK<a>');
+		$('#username').val('');
+		$('#password').val('');
+		$('#msguser').html('').hide();
+		$('.panel').hide();
 
-	chrome.storage.local.get(['ip', 'port'], function(result) {
-		ipStorage = result['ip'];
-		portStorage = result['port'];
-
-		$.getJSON(ipStorage + ":" + portStorage + "/mediabrowser/Users/Public" + jsonf, function(data) {
+		// Container for userImage
+		var userItems = [];
 			
-			// Correctly display when div is last loaded
-			$('#server-login').hide();
-			// Reset getUser and userSelect/manualLogin divs
-			$('#userSelect').html('');
-			$('#header_signIn').html('<a id="back_ipSetup">BACK<a>');
-			$('#username').val('');
-			$('#password').val('');
-			$('#msguser').html('');
-			$('.panel').hide();
-
-			// Container for userImage
-			var userItems = [];
+		$.each(data, function(key, val) {
+			
+			// Display user if enabled and not hidden
+			if (val.Configuration.IsDisabled===false && val.Configuration.IsHidden===false) {
 				
-			$.each(data, function(key, val) {
+				var userImage;
+				var userPass;
 				
-				// Display user if enabled and not hidden
-				if (val.Configuration.IsDisabled===false && val.Configuration.IsHidden===false) {
-					
-					var userImage;
-					var userPass;
-					
-					// Verify is there's a user image
-					if (typeof(val.PrimaryImageTag) != 'undefined') {
-						userImage = "background-image:url('"+ ipStorage +":"+ portStorage +"/mediabrowser/Users/"+val.Id+"/Images/Primary?width=100&tag="+val.PrimaryImageTag+"')";
-						// Add images to the userItems array
-						userItems.push("<a><div class=\"posterItemImage\" style=\"" + userImage + "\"></div><div class=\"posterItemText\">" + val.Name + "</div></a>");
-					} else {
-						// Default image for undefined
-						userImage = "background-image:url(/css/images/userflyoutdefault.png)";
-						// Add default images to the userItems array
-						userItems.push("<a><div class=\"posterItemImage\" style=\"" + userImage + "\"></div><div class=\"posterItemText\">" + val.Name + "</div></a>");
-					}
+				// Verify is there's a user image
+				if (typeof(val.PrimaryImageTag) != 'undefined') {
+					userImage = "background-image:url('"+ ipStorage +":"+ portStorage +"/mediabrowser/Users/"+val.Id+"/Images/Primary?width=100&tag="+val.PrimaryImageTag+"')";
+					// Add images to the userItems array
+					userItems.push("<a><div class=\"posterItemImage\" style=\"" + userImage + "\"></div><div class=\"posterItemText\">" + val.Name + "</div></a>");
+				} else {
+					// Default image for undefined
+					userImage = "background-image:url(/css/images/userflyoutdefault.png)";
+					// Add default images to the userItems array
+					userItems.push("<a><div class=\"posterItemImage\" style=\"" + userImage + "\"></div><div class=\"posterItemText\">" + val.Name + "</div></a>");
 				}
-			});
-
-			// Create a div userItems that contains users
-			$( "<div/>", {
-				"class": "userItems",
-				html: userItems.join( "" )
-			}).appendTo( "#userSelect");
-
-			// slideToggle
-			$('#manualLogin_text').unbind('click');
-			$('#manualLogin_text').on('click', function() {
-
-				$('.panel').slideToggle();       
-			}); 
-
-			// When pressing the save button
-			$('#saveUser').unbind('click');
-			$('#saveUser').on('click', function() {
-
-				// Authenticate the user's credentials5
-				loginUser();
-			});
-
-			// When pressing the back button
-			$('#back_ipSetup').unbind('click');
-			$('#back_ipSetup').on('click', function() {
-
-				$("#userSelect, #manualLogin").fadeOut(function() {
-					
-					// Send user back to setup IP
-					ipSetup();	
-				});
-			});
-
-			// Fancy
-			$("#userSelect, #manualLogin").fadeIn('slow');
+			}
 		});
+
+		// Create a div userItems that contains users
+		$( "<div/>", {
+			"class": "userItems",
+			html: userItems.join( "" )
+		}).appendTo( "#userSelect");
+
+		// slideToggle
+		$('#manualLogin_text').unbind('click');
+		$('#manualLogin_text').on('click', function() {
+
+			$('.panel').slideToggle();       
+		}); 
+
+		// When pressing the save button
+		$('#saveUser').unbind('click');
+		$('#saveUser').on('click', function() {
+
+			// Authenticate the user's credentials5
+			loginUser();
+
+		});
+
+		// When pressing the back button
+		$('#back_ipSetup').unbind('click');
+		$('#back_ipSetup').on('click', function() {
+
+			$("#userSelect, #manualLogin").fadeOut(function() {
+				
+				// Send user back to setup IP
+				ipSetup();	
+			});
+		});
+
+		// Fancy
+		$("#userSelect, #manualLogin").fadeIn('slow');
 	});
 }
 
@@ -222,23 +207,10 @@ function loginUser() {
 				}
 			}
 		});
-
-		// Go to Today's upcoming
-		$('#userSelect, #manualLogin').fadeOut('slow', function() {
-			todayUp();
-		});
   	
 	}).fail(function(){
-		message('#msguser', "Wrong username or password.");
+		$('#msguser').html("Wrong username or password.").show();
 	});
-}
-
-function todayUp() {
-	
-
-	current = 'todayUp';
-	chrome.storage.local.set({ 'current': current });
-
 }
 
 function logoutUser() {
